a3582a48715503c79df6c39d20995e830c59f199
Mark <125941419+Rondepart@users.noreply.github.com>
2026-08-28 22:02:31 +0800

    Add displayTodo DOM function & fix html structure

--- src/todoListDOM.js
+++ src/todoListDOM.js
@@ -14,3 +14,31 @@ export function displayProjects(projects) {
         sidebarProject.appendChild(projectListItem);
     });
 }
+
+export function displayTodo(tasks) {
+    const todoSection = document.querySelector('.todolist-section');
+    todoSection.innerHTML = '';
+
+    Object.values(tasks).forEach(task => {
+        const taskCard = document.createElement('div');
+        const taskTitle = document.createElement('h3');
+        const taskDescription = document.createElement('p');
+        const taskDueDate = document.createElement('time');
+        const taskPriority = document.createElement('div');
+        const taskNote = document.createElement('p');
+
+        taskCard.classList.add('task-card');
+        taskCard.dataset.projectGroupId = task.projectGroupID;
+
+        taskTitle.textContent = task.title;
+        taskDescription.textContent = task.description;
+        taskDueDate.textContent = task.dueDate;
+        taskPriority.textContent = task.priority;
+        taskNote.textContent = task.note;
+
+        taskCard.append(taskTitle, taskDescription, taskDueDate, taskPriority, taskNote);
+        todoSection.appendChild(taskCard);
+
+        console.log(`title:${task.title}, description:${task.description}`);
+    });
+}
