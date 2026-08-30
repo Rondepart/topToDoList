8261bf6302be615b5d20e6aede0aff57a5106341
Mark <125941419+Rondepart@users.noreply.github.com>
2026-08-31 01:50:38 +0800

    Add proj modal listnr feat & upd sidebar listnr

--- src/todoListController.js
+++ src/todoListController.js
@@ -3,11 +3,32 @@ import * as listDOM from './todoListDOM.js';
 
 export function initSidebarEvents() {
     const sidebarProjects = document.querySelector('.sidebar-projects');
+    const addProjectModal = document.querySelector('#add-project');
+    const addProjectForm = document.querySelector('.add-project-form');
+    const projectTitleInput = document.querySelector('.project-title');
 
     sidebarProjects.addEventListener('click', function(e) {
-        if(!e.target.classList.contains('project-btn')) {
+        const projectBtn = e.target.classList.contains('project-btn');
+        if(!projectBtn) {
             console.log('You did not click a project button');
+            return;
         }
-        listDOM.displayTodo(listManager.getProjectTasks(e.target.dataset.projectId));
+
+        const projectTasks = listManager.getProjectTasks(e.target.dataset.projectId);
+        listDOM.displayTodo(projectTasks);
+    });
+
+    addProjectForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const projectTitle = projectTitleInput.value;
+
+        if(projectTitle === '') return;
+
+        listManager.createProject(projectTitle);
+
+        const projects = listManager.getProjects();
+        listDOM.displayProjects(projects);
+
+        addProjectModal.close();
     });
 }
