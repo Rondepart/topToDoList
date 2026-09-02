71a933a2149b3eaebfe0c76a8b36243b91ee3c24
Mark <125941419+Rondepart@users.noreply.github.com>
2026-09-02 23:52:21 +0800

    Add delete button with svg icon in task cards

--- src/todoListDOM.js
+++ src/todoListDOM.js
@@ -58,6 +58,7 @@ export function displayTodo(tasks) {
         const taskDueDate = document.createElement('time');
         const taskPriority = document.createElement('div');
         const taskNote = document.createElement('p');
+        const dltTaskBtn = document.createElement('button');
 
         taskCard.classList.add('task-card');
         taskCard.dataset.projectGroupId = task.projectGroupID;
@@ -66,6 +67,7 @@ export function displayTodo(tasks) {
         taskDueDate.classList.add('task-card-duedate');
         taskPriority.classList.add('task-card-priority');
         taskNote.classList.add('task-note');
+        dltTaskBtn.classList.add('dlt-task-btn');
 
         taskTitle.textContent = task.title;
         taskDescription.textContent = task.description;
@@ -73,8 +75,9 @@ export function displayTodo(tasks) {
         taskDueDate.setAttribute('datetime', task.dueDate);
         taskPriority.textContent = priorityLabels[task.priority];
         taskNote.textContent = task.note;
+        dltTaskBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
 
-        taskCard.append(taskTitle, taskDescription, taskDueDate, taskPriority, taskNote);
+        taskCard.append(taskTitle, taskDescription, taskDueDate, taskPriority, taskNote, dltTaskBtn);
         todoSection.appendChild(taskCard);
 
     });
