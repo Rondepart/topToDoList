ee2633a98a83de944a280e7bb10c97676c4f5a65
Mark <125941419+Rondepart@users.noreply.github.com>
2026-09-03 21:00:20 +0800

    Add edit button with svg icon in task cards

--- src/todoListDOM.js
+++ src/todoListDOM.js
@@ -59,6 +59,7 @@ export function displayTodo(tasks) {
         const taskPriority = document.createElement('div');
         const taskNote = document.createElement('p');
         const dltTaskBtn = document.createElement('button');
+        const editTaskBtn = document.createElement('button');
 
         taskCard.classList.add('task-card');
         taskCard.dataset.projectGroupId = task.projectGroupID;
@@ -67,7 +68,11 @@ export function displayTodo(tasks) {
         taskDueDate.classList.add('task-card-duedate');
         taskPriority.classList.add('task-card-priority');
         taskNote.classList.add('task-note');
+        editTaskBtn.classList.add('edit-task-btn');
+        editTaskBtn.title = 'Edit Task';
+        editTaskBtn.dataset.taskId = task.taskID;
         dltTaskBtn.classList.add('dlt-task-btn');
+        dltTaskBtn.title = 'Delete Task';
         dltTaskBtn.dataset.taskId = task.taskID;
 
         taskTitle.textContent = task.title;
@@ -76,9 +81,10 @@ export function displayTodo(tasks) {
         taskDueDate.setAttribute('datetime', task.dueDate);
         taskPriority.textContent = priorityLabels[task.priority];
         taskNote.textContent = task.note;
-        dltTaskBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
+        dltTaskBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
+        editTaskBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z" /></svg>'
 
-        taskCard.append(taskTitle, taskDescription, taskDueDate, taskPriority, taskNote, dltTaskBtn);
+        taskCard.append(taskTitle, taskDescription, taskDueDate, taskPriority, taskNote, editTaskBtn, dltTaskBtn);
         todoSection.appendChild(taskCard);
 
     });
